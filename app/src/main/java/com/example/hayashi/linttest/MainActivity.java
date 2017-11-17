9f7274cde44beb6a9f12f2e485f423be42bb11f3
Shota Hayashi <qsezy1@hotmail.com>
2017-11-17 19:55:57 +0900

    フィールドをthis無しで呼び出し

--- app/src/main/java/com/example/hayashi/linttest/MainActivity.java
+++ app/src/main/java/com/example/hayashi/linttest/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = "test";
     }
 }
